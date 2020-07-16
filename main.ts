7b96ba8928450a8d3ed4052b777eacd702ea7765
vakiletoa <67943344+vakiletoa@users.noreply.github.com>
2020-07-16 13:29:38 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,11 +9,11 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     led.unplot(posX, 0)
     posX += 1
-    if (posX > 4) {
-        posX = 4
-    }
     led.plot(posX, 0)
+    if (posX > 4) {
+        led.plot(0, 1)
+    }
 })
 let posX = 0
-posX = 2
+posX = 0
 led.plot(posX, 0)
