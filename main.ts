641e11fabbfe3fdd46dce1b024ae435c0edfe98d
vakiletoa <67943344+vakiletoa@users.noreply.github.com>
2020-07-16 16:42:37 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,29 @@
 input.onButtonPressed(Button.A, function () {
-    led.unplot(posX, 0)
+    led.unplot(posX, posY)
     posX += -1
     if (posX < 0) {
-        posX = 0
+        posX = 4
+        posY += -1
+        if (posY < 0) {
+            posY = 4
+        }
     }
-    led.plot(posX, 0)
+    led.plot(posX, posY)
 })
 input.onButtonPressed(Button.B, function () {
-    led.unplot(posX, 0)
+    led.unplot(posX, posY)
     posX += 1
-    led.plot(posX, 0)
     if (posX > 4) {
-        led.plot(0, 1)
+        posX = 0
+        posY += 1
+        if (posY > 4) {
+            posY = 0
+        }
     }
+    led.plot(posX, posY)
 })
+let posY = 0
 let posX = 0
-posX = 0
-led.plot(posX, 0)
+posX = 2
+posY = 2
+led.plot(posX, posY)
